01c8dbf3dcbf53801ee8d72ed028d1e1463c6301
Sreesanth M <73953412+SREESANTHwWw@users.noreply.github.com>
2025-01-09 03:36:40 +0530

    NavBar

--- Client/src/Pages/CarouselPage.jsx
+++ Client/src/Pages/CarouselPage.jsx
@@ -5,11 +5,11 @@ import rewind from '../assets/rewind.jpg';
 const CarouselPage = () => {
   const slides = [
     {
-      imgSrc: brushcutter,
+      imgSrc: "https://res.cloudinary.com/dkz8fh4jt/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,l_text:montserrat_25_style_light_align_center:Shop%20Now,w_0.5,y_0.18/v1736373530/drde5c8uvnsftbtji5zw.jpg",
       imgAlt: '',
     },
     {
-      imgSrc: rewind,
+      imgSrc: "https://res.cloudinary.com/dkz8fh4jt/image/upload/v1736373530/drde5c8uvnsftbtji5zw.jpg",
       imgAlt: 'rewind',
     },
     {
